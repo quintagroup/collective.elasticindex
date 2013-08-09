2c7ed838b4d54d38c83de010cbcee3256453b7ff
Job van Achterberg <job@infrae.com>
2013-08-09 11:31:36 +0200

    document mapping: extended sensical field mappings, plus desired fields

--- src/collective/elasticindex/utils.py
+++ src/collective/elasticindex/utils.py
@@ -4,12 +4,45 @@ import urlparse
 
 from collective.elasticindex.interfaces import IElasticSettings
 
+ANALYZED_STRING_MAPPING = {
+    'index'    : 'analyzed',
+    'type'     : 'string', 
+    'store'    : 'yes', 
+    'analyzer' : 'standard'
+}
 
-MAPPINGS = {
-    'title': {'type': 'string', 'store': 'yes', 'index': 'analyzed'},
-    'url': {'type': 'string', 'store': 'yes', 'index': 'analyzed'},
-    'content': {'type': 'string',  'store': 'yes', 'index': 'analyzed'}}
+STRING_MAPPING = {
+    'index'    : 'not_analyzed',
+    'type'     : 'string', 
+    'store'    : 'yes', 
+}
 
+DATE_MAPPING = {
+    'index' : 'not_analyzed',
+    'type'  : 'date',
+    'store' : 'no'
+}
+
+DOCUMENT_MAPPING = {
+    '_index' : {
+        'enabled' : True
+    },
+    '_id' : {
+        'index' : 'not_analyzed',
+        'store' : 'yes'
+    },
+
+    'site'    : STRING_MAPPING,
+
+    'url'     : ANALYZED_STRING_MAPPING,
+    'author'  : ANALYZED_STRING_MAPPING,
+    'title'   : ANALYZED_STRING_MAPPING,
+    'subject' : ANALYZED_STRING_MAPPING,
+    'content' : ANALYZED_STRING_MAPPING,
+    
+    'created'  : DATE_MAPPING,
+    'modified' : DATE_MAPPING 
+}
 
 def parse_url(url):
     info = urlparse.urlparse(url)
@@ -34,5 +67,5 @@ def create_index(settings):
     connection = connect(settings.server_urls)
     connection.indices.create_index_if_missing(settings.index_name)
     connection.indices.put_mapping(
-        'document', {'properties':MAPPINGS}, [settings.index_name])
+        'document', {'properties' : DOCUMENT_MAPPING}, [settings.index_name])
 
