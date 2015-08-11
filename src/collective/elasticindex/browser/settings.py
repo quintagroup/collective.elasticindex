90a358ddff7010630f0f2d71cb28827ad9a49876
Taras Kozlovskyi <ktarasz@quintagroup.com>
2015-08-11 12:30:17 +0000

    Fix controlpanel

--- src/collective/elasticindex/browser/settings.py
+++ src/collective/elasticindex/browser/settings.py
@@ -1,8 +1,8 @@
 
 from Products.statusmessages.interfaces import IStatusMessage
-from plone.app.controlpanel.form import ControlPanelForm
-from zope.formlib import form
 from zope.i18nmessageid import MessageFactory
+from z3c.form import form, field, button
+from plone.autoform.form import AutoExtensibleForm
 
 from collective.elasticindex.interfaces import IElasticSettings
 from collective.elasticindex.changes import changes
@@ -10,16 +10,18 @@ from collective.elasticindex.utils import create_index, delete_index
 
 
 _ = MessageFactory('collective.elasticindex')
+_plone = MessageFactory('plone')
 
 
-class ElasticIndexSettings(ControlPanelForm):
+class ElasticIndexSettings(AutoExtensibleForm, form.EditForm):
     label = _("Elasticsearch configuration")
     description = _("Configure elasticsearch index.")
     form_name = _("Elasticsearch configuration")
-    form_fields = form.Fields(IElasticSettings)
+    control_panel_view = "plone_control_panel"
+    schema = IElasticSettings
 
-    @form.action('Create index', name='create_index')
-    def create_index(self, action, data):
+    @button.buttonAndHandler(u'Create index', name='create_index')
+    def create_index(self, action):
         send = IStatusMessage(self.request).add
         try:
             create_index(IElasticSettings(self.context))
@@ -28,8 +30,8 @@ class ElasticIndexSettings(ControlPanelForm):
         else:
             send("Index created.")
 
-    @form.action('Delete index', name='delete_index')
-    def delete_index(self, action, data):
+    @button.buttonAndHandler(u'Delete index', name='delete_index')
+    def delete_index(self, action):
         send = IStatusMessage(self.request).add
         try:
             delete_index(IElasticSettings(self.context))
@@ -38,8 +40,8 @@ class ElasticIndexSettings(ControlPanelForm):
         else:
             send("Index deleted.")
 
-    @form.action('Import site content', name='import_content')
-    def import_site_content(self, action, data):
+    @button.buttonAndHandler(u'Import site content', name='import_content')
+    def import_site_content(self, action):
         send = IStatusMessage(self.request).add
         try:
             changes.verify_and_index_container(self.context)
@@ -48,4 +50,24 @@ class ElasticIndexSettings(ControlPanelForm):
         else:
             send("Index refreshed.")
 
-    actions += form.Actions(*list(ControlPanelForm.actions))
+    @button.buttonAndHandler(_plone(u"Save"), name='save')
+    def handleSave(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        self.applyChanges(data)
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"Changes saved."),
+            "info")
+        self.request.response.redirect(self.request.getURL())
+
+    @button.buttonAndHandler(_plone(u"Cancel"), name='cancel')
+    def handleCancel(self, action):
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"Changes canceled."),
+            "info")
+        self.request.response.redirect("%s/%s" % (
+            self.context.absolute_url(),
+            self.control_panel_view))
+
